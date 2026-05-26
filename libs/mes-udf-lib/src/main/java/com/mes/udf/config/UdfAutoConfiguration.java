c5b0f86d311cdba19d2b99037b00c7f2b3f1f152
artical2k9 <75570583+artical2k9@users.noreply.github.com>
2026-05-26 08:39:20 +0100

    [chore](MES-8): add UdfAutoConfiguration stub and Spring factories entry [T012]
    Ref: MES-8
    Task: T012

---
+++ libs/mes-udf-lib/src/main/java/com/mes/udf/config/UdfAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.mes.udf.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "com.mes.udf")
+public class UdfAutoConfiguration {
+}
