4779b3d6ba9e169cb00864a27f240b6f23148044
artical2k9 <75570583+artical2k9@users.noreply.github.com>
2026-06-04 17:44:19 +0200

    [feat](MES-111): copy ItemMasterService and exceptions to com.mes.inventory.itemmaster.service [T027]
    Ref: MES-111
    Task: T027

---
+++ services/inventory-service/src/main/java/com/mes/inventory/itemmaster/service/ItemMasterValidationException.java
@@ -0,0 +1,7 @@
+package com.mes.inventory.itemmaster.service;
+
+public class ItemMasterValidationException extends RuntimeException {
+    public ItemMasterValidationException(String message) {
+        super(message);
+    }
+}
